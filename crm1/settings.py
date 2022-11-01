edbc250a7abf21de1c342785e2724bba3a457e9f
Hariharan S <93510043+hari0805@users.noreply.github.com>
2022-11-01 15:03:38 +0530

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES[default].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
